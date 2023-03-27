b2ace923c9125e80293da482c290b35dd65780ac
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-03-28 04:26:39 +0700

    update lab12

--- CS464H_TRANCONGTRI/Lab11/Main.Designer.cs
+++ CS464H_TRANCONGTRI/Lab11/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab11
 {
-	partial class Form1
+	partial class Main
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -28,7 +28,7 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
 			this.label5 = new System.Windows.Forms.Label();
 			this.textBox3 = new System.Windows.Forms.TextBox();
 			this.rdo_GiamDan = new System.Windows.Forms.RadioButton();
--- CS464H_TRANCONGTRI/Lab12/Program.cs
+++ CS464H_TRANCONGTRI/Lab12/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab11
+namespace Lab12
 {
 	static class Program
 	{
@@ -16,7 +16,7 @@ namespace Lab11
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Main());
+			Application.Run(new DangNhap());
 		}
 	}
 }
